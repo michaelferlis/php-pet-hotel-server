a98b5a42ffe1b0024083793cd1a01b977bf1072b
Trevor Ramlow <trevorramlow@gmail.com>
2019-07-18 11:34:52 -0500

    changed the file structure

---
+++ delete-owner/index.php
@@ -0,0 +1,24 @@
+<?php
+ $dbconn = pg_connect("host=localhost dbname=pet_hotel")
+    or die('Could not connect: ' . pg_last_error());
+    $petArray = array(
+        "pet_id" => 1
+    )
+    $ownerArray = array(
+        "owner_id" => 1
+    );
+// Performing SQL query
+$res = pg_delete($dbconn, "pets", $petArray);
+if ($res) {
+    echo "POST data is deleted: $res\n";
+} else {
+    echo "User must have sent wrong inputs\n";
+}
+$res = pg_delete($dbconn, "owner", $ownerArray);
+if ($res) {
+    echo "POST data is deleted: $res\n";
+} else {
+    echo "User must have sent wrong inputs\n";
+}
+?> 
+
