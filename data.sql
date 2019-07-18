ebaf529c796237a9e40232e034aea56ff6b70170
Michael Ferlis <michaelferlis@gmail.com>
2019-07-18 09:57:15 -0500

    add db.sql file

---
+++ data.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "owners" (
+    "owner_id" SERIAL PRIMARY KEY,
+    "owner_name" VARCHAR (80) UNIQUE NOT NULL,
+    "number_of_pets" INT NOT NULL
+);
+
+CREATE TABLE "pets" (
+    "pet_id" SERIAL PRIMARY KEY,
+    "pet_name" VARCHAR(100),
+    "breed" VARCHAR(1000),
+    "color" VARCHAR(1000),
+    "checked_in" text DEFAULT 'true'::text,
+    "pet_owner_id" INT REFERENCES "owners"
+    
+);
